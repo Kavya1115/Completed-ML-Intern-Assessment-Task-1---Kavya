fc2039db5f317bc5bdb7ac5dcbb2b13e0aa0f46b
Kavya1115 <ekavya24311@gmail.com>
2025-11-21 18:37:52 +0530

    Delete ml-intern-assessment-main directory

--- ml-intern-assessment-main/trigram-assignment/src/utils.py
+++
@@ -1,8 +0,0 @@
-# This file is optional.
-# You can add any utility functions you need for your implementation here.
-import re
-
-def tokenize(text):
-    text = text.lower()
-    return re.findall(r"\b\w+\b", text)
-
